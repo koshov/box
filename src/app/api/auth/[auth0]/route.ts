75da87e787b259e78e3252e11727e6f4d92bb399
Georgi <koshov@gmail.com>
2025-06-19 13:20:07 +0300

    Clean up auth

--- src/app/api/auth/[auth0]/route.ts
+++
@@ -1,8 +0,0 @@
-import { handleAuth, handleLogin, handleLogout, handleCallback, handleProfile } from '@auth0/nextjs-auth0';
-
-export const GET = handleAuth({
-  login: handleLogin(),
-  logout: handleLogout(),
-  callback: handleCallback(),
-  'me': handleProfile()
-});
