6118c70542093378df968235f571b4b75a7484aa
Georgi <koshov@gmail.com>
2025-06-19 12:55:33 +0300

    Basic auth

---
+++ src/app/api/auth/[auth0]/route.ts
@@ -0,0 +1,8 @@
+import { handleAuth, handleLogin, handleLogout, handleCallback, handleProfile } from '@auth0/nextjs-auth0';
+
+export const GET = handleAuth({
+  login: handleLogin(),
+  logout: handleLogout(),
+  callback: handleCallback(),
+  'me': handleProfile()
+});
